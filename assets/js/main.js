d158e99ed983d413c0d21226d740c2804a8bb817
Gabriel Leandro <102603536+GabrielV26@users.noreply.github.com>
2023-05-20 18:34:16 -0300

    Refatorei com a função map
    Adicionei o código JavaScript que utiliza a função map para percorrer a lista de pokémons retornada pela função getPokemons da API da PokeAPI. Cada pokémon é convertido em um elemento <li> utilizando a função convertPokemonToLi, e os elementos resultantes são concatenados em uma nova string de HTML. Essa string é então adicionada à lista de pokémons na página.

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,13 +24,14 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons) => {
-    const listItems = []
+pokeApi.getPokemons().then((pokemons = []) => {
+
+    const newList = pokemons.map((pokemon) => {
+        return convertPokemonToLi(pokemon)
+    })
+
+    const newHtml = newList.join('')
+
+    pokemonList.innerHTML += newHtml
     
-    for(let i = 0 ; i < pokemons.length; i++){
-        const pokemon = pokemons[i];
-        listItems.push(convertPokemonToLi(pokemon))
-    }
-    
-    console.log(listItems)
 })
